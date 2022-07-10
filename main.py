ec42482a66e72f875ea5ecb29a56aead4aa09b7b
girnal82 <grinal82@gmail.com>
2022-07-10 19:49:13 +0300

    executed phone formatting though 1 itteration

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import re
 def reader():
     # reading up the csv file to get the data to work with
     file_name = "phonebook_raw.csv"
-    # my_list = []
     with open(file_name, encoding='utf-8') as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
@@ -52,9 +51,7 @@ def splitter(contacts_list):
                     i[5] = j[5]
                 if i[6] == '':
                     i[6] = j[6]
-        for el in i:
-            if i.index(el) > 6:
-                i.remove(el)
+        # removing double rows
         if my_list.count(i) > 1:
             my_list.remove(i)
     return my_list
@@ -66,39 +63,25 @@ def phone_formatter(my_list):
     for row in my_list:
         text = ",".join(row)
         pattern = re.compile(
-            r"(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\s*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)")
+            r"(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)")
         result = pattern.sub(r"+7(\4)\8-\11-\14\15\17\18\20", text)
         new_text = result.split(',')
         formatted_list.append(new_text)
     return formatted_list
 
 
-def final_formatter(formatted_list):
-    # formatting the remaining phone number with a specifically written pattern
-    final_formatted_list = []
-    for row in formatted_list:
-        text = ",".join(row)
-        patten2 = re.compile(r"(8)\s(\d{3})-(\d{3})-(\d{2})(\d{2})")
-        result = patten2.sub(r"+7(\2)\3-\4-\5", text)
-        new_text = result.split(',')
-        final_formatted_list.append(new_text)
-    # pprint(final_formatted_list)
-    return final_formatted_list
-
-
-def writer(final_formatted_list):
+def writer(formatted_list):
     # writing down editted phonebook into a newly created csv file
     with open("phonebook.csv", "w", encoding='utf-8') as f:
         datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows(final_formatted_list)
+        datawriter.writerows(formatted_list)
 
 
 def main():
     contacts_list = reader()
     my_list = splitter(contacts_list)
     formatted_list = phone_formatter(my_list)
-    final_formatted_list = final_formatter(formatted_list)
-    writer(final_formatted_list)
+    writer(formatted_list)
 
 
 if __name__ == '__main__':
